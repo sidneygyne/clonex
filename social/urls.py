fa6ca2a65ab694f003cbbc52ab01a4c6ff5e6fa6
sidneygyne <sidneyr_gyn@hotmail.com>
2025-11-23 09:45:09 -0300

    Endpoints de seguir/deixar de seguir

--- social/urls.py
+++ social/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
-urlpatterns = []
+from .views import FollowToggleView, FollowersListView, FollowingListView
+
+urlpatterns = [
+    path("follow/<int:user_id>/", FollowToggleView.as_view(), name="follow-toggle"),
+    path("followers/<int:user_id>/", FollowersListView.as_view(), name="followers-list"),
+    path("following/<int:user_id>/", FollowingListView.as_view(), name="following-list"),
+]
